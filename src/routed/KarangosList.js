9115c77226067ee224b6e7d53c5b9740c96eb40b
Thiago Ferrari <thiagofcjr@gmail.com>
2021-05-20 17:17:21 -0300

    atualizações APP

--- src/routed/KarangosList.js
+++ src/routed/KarangosList.js
@@ -1,5 +1,28 @@
+import axios from "axios"
+import { useEffect, useState } from "react"
+
 export default function KarangosList() {
+    // é importante que este estado seja setado com um array vazio
+    const [karangos, setKarangos] = useState([])
+
+    useEffect(() => {
+        const getData = async () => {
+            try {
+                let response = await axios.get('https://api.faustocintra.com.br/karangos')
+                setKarangos(response.data)
+            } catch (error) {
+                console.error(error)
+            }
+        }
+        getData()
+    }, []) // dependências vazias, esse useEffect executa só uma vez
+
     return (
-        <h1>Listagem de karangos</h1>
+        <>
+            <h1>Listagem de karangos</h1>
+            {
+                karangos.map(karango => <div key={karango.id}>{karango.modelo}</div>)
+            }
+        </>
     )
 }
